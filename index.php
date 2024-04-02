ec2a7c39573990e74ae1f51dd42e5999c41cb564
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-04-01 22:19:09 -0300

    site de pizzaria só que no php

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzaria</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <header class="row cabecalho">
+            <div class="col">
+                <img src="logotipo.fw.png" alt="Super pizza" class="img-fluid">
+            </div>
+            <div col text-end>
+                <h1 class="text-light h2">Disk Entregas<br>(19) 1234-5678</h1>
+            </div>
+        </header>
+        <nav class="row menu ps-3 pe-3 pt-2 pb-2">
+            <div class="col">
+                <a href="index.php" class="me-2">Home</a>
+                <a href="quem-somos.php" class="me-2">Quem Somos</a>
+                <a href="contato.php" class="me-2">Contato</a>
+            </div>
+        </nav>
+        <main class="bg-light row pe-3 ps-3">
+            <div class="row row pe-3 ps-3">
+                <h2 class="mt-4 h1"> A melhor Pizza da região</h2>
+                <hr>
+                <div class="col">
+                    <img src="calabreza.jpg" alt="Calabreza">
+                    <p>Calabreza</p>
+                </div>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h3>Nossos Sabores</h3>
+                    <p class="sabores">Á Moda da Casa <br>
+                        aliche <br>
+                        Atum <br>
+                        Baiana <br>
+                        Bauru <br>
+                        Canadense <br>
+                        Catupiry <br>
+                        Á Moda da Casa <br>
+                        aliche <br>
+                        Atum <br>
+                        Baiana <br>
+                        Bauru <br>
+                        Canadense <br>
+                        Catupiry <br>
+                        Calabresa <br>
+                    </p>
+                </div>
+                <div class="col">
+                    <h3>Uma casa com história</h3>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="historia.png" class="img-fluid" alt="historia">
+                        </div>
+                        <div class="col">
+                            <p>Muito famosa em toda a região por sua excelêcia na qualidade, a nossa pizzaria tem mais de 25 anos de história</p>
+                            <p>
+                                <a href="quem-somos" class="link-destaque">Conheça</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer class="row rodape ps-3 pe-3 pt-3">
+            <div class="col">
+                <p class="text-light fw-lighter">Aula do Senac. Declaração dos direitos autorais</p>
+            </div>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ quem-somos.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzaria</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <header class="row cabecalho p-3">
+            <div class="col">
+                <img src="logotipo.fw.png" alt="Super pizza" class="img-fluid">
+            </div>
+            <div col text-end>
+                <h1 class="text-light h2">Disk Entregas<br>(19) 1234-5678</h1>
+            </div>
+        </header>
+        <nav class="row menu ps-3 pe-3 pt-2 pb-2">
+            <div class="col">
+                <a href="index.php" class="me-2">Home</a>
+                <a href="quem-somos.php" class="me-2">Quem Somos</a>
+                <a href="contato.php" class="me-2">Contato</a>
+                <a href="pedidos.php" class="me-2">FAÇA SEU PEDIDO</a>
+            </div>
+        </nav>
+        <main class="bg-light row pe-3 ps-3">
+            <div class="row">
+                <h2 class="mt-4 h1">A melhor pizza da região</h2>
+                <hr>
+                <div class="col">
+                    <p>No final dos anos 1950, quando a cidade ainda exalava os aromas da imigração italiana, a família Rossi decidiu trazer um pedacinho da Nápoles para o Brasil. E assim, em 1958, nascia a Super Pizza Pan, em um charmoso casarão na Rua Treze de Maio, 1452. A massa macia, os ingredientes frescos e o amor pela tradição italiana eram os segredos por trás de cada fatia.</p>
+                    <p>O nome “Super Pizza Pan” não foi escolhido por acaso. As pizzas aqui são preparadas em formas redondas, como verdadeiras rodas de sabor. Os italianos que chegaram a São Paulo trouxeram consigo a paixão pela pizza, e a Super Pizza Pan se tornou um ícone dessa tradição. Os sabores clássicos, como pepperoni e frango Philadelphia, são celebrados por clientes fiéis e novos visitantes.</p><br>
+                    
+                </div>
+            </div>
+        </main>
+        <footer class="row rodape ps-3 pe-3 pt-3">
+            <div class="col">
+                <p class="text-light fw-lighter">Aula do Senac. Declaração dos direitos autorais</p>
+            </div>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
